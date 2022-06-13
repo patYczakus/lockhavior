06daa1e642cc4c388d304b9743ed0e4fcfae2bc6
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2022-06-13 19:43:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+import { app } from "./database/database.config.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js"
+var body = document.body
+let auth = getAuth()
+
+window.onload = () => {
+    body.innerHTML = `<input id="email" type="email" placeholder="email" /><input id="haslo" type="password" placeholder="hasło" /><br />
+    <button id="submit">Potwierdź</button>`
+
+    document.getElementById("submit").addEventListener("click", () => {
+        if (!document.getElementById("email").value || !document.getElementById("haslo").value) return alert("Brak emaila/hasła!")
+
+        signInWithEmailAndPassword(auth, document.getElementById("email").value, document.getElementById("haslo").value)
+            .then((response) => {
+                console.log(response.user)
+            })
+            .catch((err) => {
+                alert(err.message)
+            })
+    })
+}
