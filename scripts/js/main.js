51efd9f131b2a6b803371255459c42f59ab5c352
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2022-10-30 21:33:17 +0100

    Aktualizacja 1.1

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -2,34 +2,39 @@ import { app } from "./database.js"
 import { getDatabase, ref, set, onValue, push, child } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js"
 import { GoogleAuthProvider, FacebookAuthProvider, signInWithPopup, getAuth } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js"
 
-var img, color, name
+var img, color, name, uid
+var ytLink = "none"
+var ytLinkNormal = null
 let arrayNumber = 0
 
-function createDiv(dKey, dColor, dImg, dName, dText, opacity) {
-    dText = dText
-        .replace(/</g, "&lt;")
-        .replace(/\n/g, "<br />")
-
-    return `<div id="${dKey}" class="boxA${opacity ? " send" : ""}" style="border-left-color: ${dColor};">
-    <div class="image"><img src="${dImg}" width="45px" height="45px" class="image" /></div>
-    <div class="boxB">
-        <div class="user">${dName}</div>
-        <div class="text">${dText}</div>
-    </div>
-    </div>`
-}
-
-function writeUserData(text = String()) {
+function writeUserData(text = String(), yt_link) {
     const db = getDatabase()
 
-    const newPostKey = push(child(ref(db), 'messages/')).key
+    var info
+    if (yt_link != "none" && yt_link != null) {
+        info = {
+            username: name,
+            profile_picture : img,
+            text: text,
+            border_color: color,
+            user_id: uid,
+            youtube_video_link: yt_link
+        }
 
-    set(ref(db, 'messages/' + newPostKey), {
-        username: name,
-        profile_picture : img,
-        text: text,
-        color: color,
-    })
+        ytLink = "none"
+        ytLinkNormal = null
+        document.querySelector("button#addYtVideo").style.background = "#d4d4d4"
+    } else {
+        info = {
+            username: name,
+            profile_picture : img,
+            text: text,
+            border_color: color,
+            user_id: uid 
+        }
+    }
+
+    set(ref(db, 'lastmessage/'), info)
 }
 
 window.onload = () => {
@@ -40,26 +45,12 @@ window.onload = () => {
         signInWithPopup(auth, provider)
             .then((result) => {
                 const user = result.user;
-                
+                console.log(user)
+
                 img = user.photoURL
                 color = "#d95333"
                 name = user.displayName
-                start()
-            }).catch((error) => {         
-                console.error(error)
-            });
-    })
-    document.querySelector("button#fb").addEventListener("click", () => {
-        const provider = new FacebookAuthProvider();
-        const auth = getAuth(app);
-        
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                const user = result.user;
-                
-                img = user.photoURL
-                color = "#1877f2"
-                name = user.displayName
+                uid = user.uid
                 start()
             }).catch((error) => {         
                 console.error(error)
@@ -70,42 +61,90 @@ window.onload = () => {
 function start() {
     document.body.innerHTML = `<div id="chat"></div>
     <div>
-        <textarea id="send" placeholder="Aa"></textarea><button id="sendBtn"><img src="src/svg-s/send_FILL0_wght400_GRAD-25_opsz24.svg" style="margin-top: 4.3px" /></button>
-        <button style="display: flex; align-items: center; font-size: 0px; background: linear-gradient(90deg, ${color} 15px, #d4d4d4 45px)">
-            <div class="image"><img src="${img}" width="45px" height="45px" class="image" /></div>
-            <div class="user" style="display: inline-block; font-size: 26px; height: auto;">${name}</div>
-            <span type="link" onclick="location.reload();">Logout</span>
+        <textarea id="send" placeholder="Aa"></textarea>
+        <button id="sendBtn"><img alt="Wyślij" src="src/svg-s/send.svg" style="margin-top: 4.3px" /></button>
+        <button id="addYtVideo"><img alt="Link do YT" src="src/svg-s/youtube_activity.svg" style="margin-top: 4.3px" /></button>
+        <div id="btn" style="
+            display: inline-flex;
+            align-items: center; 
+            font-size: 0px; 
+            background: linear-gradient(90deg, ${color} 15px, #d4d4d4 45px);
+            -webkit-tap-highlight-color: transparent;
+        "><div class="image"><img src="${img}" width="45px" height="45px" class="image" /></div>
+            <div class="user" style="display: inline-block; font-size: 26px; height: auto; margin-right: 10px">${name}</div>
+            <button class="small" onclick="location.reload();"><img alt="Wyloguj" src="src/svg-s/logout.svg"/></span>
         </button>
     </div>`
 
     document.querySelector("button#sendBtn").addEventListener("click", () => {
         if (!document.querySelector("textarea#send").value.trim().length) return;
-        writeUserData(document.querySelector("textarea#send").value)
+        writeUserData(document.querySelector("textarea#send").value, ytLink)
         document.querySelector("textarea#send").value = ""
+    })
+    document.querySelector("button#addYtVideo").addEventListener("click", () => {
+        ytLink = prompt(`Podaj link do wideo${ytLinkNormal != null ? "\nWyczyść, aby usunąć link": ""}`, ytLinkNormal != null ? ytLinkNormal : "")
+        if (ytLink == null || ytLink == "") { 
+            ytLink == "none" 
+            ytLinkNormal = null
+            document.querySelector("button#addYtVideo").style.background = "#d4d4d4"
+        }
+        else if (ytLink.startsWith("https://youtu.be/") || ytLink.startsWith("https://www.youtube.com/watch")) {
+            ytLinkNormal = ytLink
+            ytLink = ytLink.replace("https://www.youtube.com/watch", "").replace("https://youtu.be/", "")
+            if (ytLink.indexOf("?t=") > -1) { 
+                ytLink = ytLink.split("?t=") 
+                ytLink = {
+                    code: ytLink[0],
+                    timer: ytLink[1]
+                }
+            } else {
+                ytLink = {
+                    code: ytLink
+                }
+            }
+
+            document.querySelector("button#addYtVideo").style.background = "#1bff00"
+        }
+        else {
+            alert("Zły link!")
+            ytLink = "none" 
+            ytLinkNormal = null
+            document.querySelector("button#addYtVideo").style.background = "#d4d4d4"
+        }
     })
     document.querySelector("textarea#send").addEventListener("keydown", (btn) => { 
         if (btn.ctrlKey && btn.code == "Enter") {
             if (!document.querySelector("textarea#send").value.trim().length) return;
-            writeUserData(document.querySelector("textarea#send").value)
+            writeUserData(document.querySelector("textarea#send").value, ytLink)
             setTimeout(document.querySelector("textarea#send").value = "", 1000)
         }
     })
 
     const db = getDatabase();
-    const msg = ref(db, 'messages');
+    const msg = ref(db, 'lastmessage/');
+    
     onValue(msg, (snapshot) => {
         var data = snapshot.val();
-        if (data == null) return document.getElementById("chat").innerText = "Nic tu nie ma, jak narazie..."
 
-        setTimeout(() => {data = Object.entries(data)
         
-        console.log(data)
-        
-        let text = ""
-        for (let i = arrayNumber; i < data.length; i++) {
-            text += createDiv(data[i][0], data[i][1].color, data[i][1].profile_picture, data[i][1].username, data[i][1].text, false) + "\n\n"
+
+        let text = ""    
+        var args = ""
+        if ("youtube_video_link" in data) { 
+            args += `<iframe width="560" height="315" src="https://www.youtube.com/embed/${data.youtube_video_link.code}{yt-timer-c34}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="margin-left: 8px; border-radius: 0.8rem; margin-top: 4px; border: 8px double black;"></iframe>` 
+            if ("timer" in data.youtube_video_link) args = args.replace("{yt-timer-c34}", `?start=${data.youtube_video_link.timer}`) 
+            else args = args.replace("{yt-timer-c34}", "")
         }
-        document.getElementById("chat").innerHTML = text}, 1)
-    });
-    
+
+        text += `<div id="${data.user_id}" class="boxA" style="border-left-color: ${data.border_color};">
+        <div class="image"><img src="${data.profile_picture}" width="45px" height="45px" class="image" /></div>
+        <div class="boxB">
+            <div class="user">${data.username}</div>
+            <div class="text">${data.text.replace(/</g, "&lt;").replace(/\n/g, "<br />")}</div>
+            ${args}
+        </div>
+        </div>`
+        text += "\n\n"
+        document.getElementById("chat").innerHTML += text
+    })
 }
